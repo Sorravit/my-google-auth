f9f276b517c614bba4629e65884568ba26e114ac
mac <mac.kittipat@gmail.com>
2016-08-27 09:14:39 +0700

    Gen OTP

--- src/main/java/com/mackittipat/mygoogleauth/App.java
+++ src/main/java/com/mackittipat/mygoogleauth/App.java
@@ -1,7 +1,30 @@
 package com.mackittipat.mygoogleauth;
 
+import org.apache.commons.io.FileUtils;
+import org.jboss.aerogear.security.otp.Totp;
+
+import java.io.File;
+import java.io.IOException;
+
 public class App {
     public static void main( String[] args ) {
-        
+
+        String filePath = args[0];
+        String secret;
+        try {
+            secret = FileUtils.readFileToString(new File(filePath), "UTF-8").trim();
+        } catch (IOException e) {
+            System.out.println("File does not exist !!!");
+            return;
+        }
+
+        Totp totp = new Totp(secret);
+        String opt = totp.now();
+        System.out.println(opt);
+
+        // TODO Copy to clipboard.
+//        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+//        Transferable transferable = new StringSelection("hello");
+//        clipboard.setContents(transferable, null);
     }
 }
